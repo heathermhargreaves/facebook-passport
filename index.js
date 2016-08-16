fb7063316df4c57db3d582dae2443a25605410f6
heathermhargreaves <heathermhargreaves@gmail.com>
2016-08-16 12:14:34 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var session = require('express-session');
+var passport = require('passport');
+var facebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new facebookStrategy({
+    clientID: "appid",
+    clientSecret: "appsecret",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, next) {
+    return next(null, profile);
+}));
+
+var app = express();
+app.use(session({secret: 'hello12345'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    successRedirect: '/me',
+    failureRedirect: '/'
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get('/me', function(req, res, next) {
+  res.send(req.user);
+});
+
+app.listen(3000);
